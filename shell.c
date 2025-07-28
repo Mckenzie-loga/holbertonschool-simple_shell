b63847c4275198662320a0260d5903f7deec81f4
Mckenzie-loga <mckenzieloga2003@gmail.com>
2025-07-28 12:15:01 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,103 +1,65 @@
-#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/* Helper to check if file is executable */
-int is_executable(char *path)
+#define BUFFER_SIZE 1024
+
+int main(void)
 {
-	struct stat st;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    int status;
 
-	if (stat(path, &st) == 0)
-	{
-		if ((st.st_mode & S_IFREG) && (access(path, X_OK) == 0))
-			return 1;
-	}
-	return 0;
-}
+    while (1)
+    {
+        printf("$ ");
+        fflush(stdout);
 
-/* Find command in PATH */
-char *find_command(char *cmd)
-{
-	char *path_env, *path_dup, *token, *full_path;
-	size_t len;
+        read = getline(&line, &len, stdin);
+        if (read == -1)  /* EOF (Ctrl+D) */
+        {
+            printf("\n");
+            break;
+        }
 
-	if (strchr(cmd, '/'))
-	{
-		if (is_executable(cmd))
-			return strdup(cmd);
-		return NULL;
-	}
+        /* Remove trailing newline */
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
 
-	path_env = getenv("PATH");
-	if (!path_env || path_env[0] == '\0')
-		return NULL;
+        if (strcmp(line, "") == 0)
+            continue;
 
-	path_dup = strdup(path_env);
-	if (!path_dup)
-		return NULL;
+        if (strcmp(line, "exit") == 0)
+            break;
 
-	token = strtok(path_dup, ":");
-	while (token)
-	{
-		len = strlen(token) + strlen(cmd) + 2;
-		full_path = malloc(len);
-		if (!full_path)
-		{
-			free(path_dup);
-			return NULL;
-		}
-		snprintf(full_path, len, "%s/%s", token, cmd);
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            continue;
+        }
+        if (pid == 0)
+        {
+            /* Child process executes the command */
+            char *argv[] = {line, NULL};
 
-		if (is_executable(full_path))
-		{
-			free(path_dup);
-			return full_path;
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
+            execve(line, argv, environ);
 
-	free(path_dup);
-	return NULL;
-}
+            /* If execve returns, there was an error */
+            perror("./simple_shell");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            /* Parent waits for child */
+            waitpid(pid, &status, 0);
+        }
+    }
 
-int execute(char **args)
-{
-	pid_t pid;
-	int status;
-	char *cmd_path;
-
-	if (args[0] == NULL)
-		return 1;
-
-	if (handle_builtin(args) == 0)
-		return 0;
-
-	cmd_path = find_command(args[0]);
-	if (!cmd_path)
-	{
-		/* Command not found, print error and do NOT fork */
-		dprintf(STDERR_FILENO, "%s: No such file or directory\n", args[0]);
-		return 1;
-	}
-
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(cmd_path, args, environ);
-		perror("execve");
-		free(cmd_path);
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-		free(cmd_path);
-		return 1;
-	}
-	else
-	{
-		free(cmd_path);
-		waitpid(pid, &status, 0);
-	}
-
-	return 1;
+    free(line);
+    return 0;
 }
