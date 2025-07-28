3e2727f43ce15eb4f142840c4a968b668a09580e
Mckenzie-loga <mckenzieloga2003@gmail.com>
2025-07-28 11:58:38 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,106 +1,103 @@
-#include "shell.h"
+#include <sys/stat.h>
 
-/**
- * prompt - Display shell prompt
- */
-void prompt(void)
+/* Helper to check if file is executable */
+int is_executable(char *path)
 {
-	write(STDOUT_FILENO, "$ ", 2);
+	struct stat st;
+
+	if (stat(path, &st) == 0)
+	{
+		if ((st.st_mode & S_IFREG) && (access(path, X_OK) == 0))
+			return 1;
+	}
+	return 0;
 }
 
-/**
- * read_line - Read a line of input from stdin
- *
- * Return: Pointer to the line
- */
-char *read_line(void)
+/* Find command in PATH */
+char *find_command(char *cmd)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
+	char *path_env, *path_dup, *token, *full_path;
+	size_t len;
 
-	nread = getline(&line, &len, stdin);
-	if (nread == -1)
+	if (strchr(cmd, '/'))
 	{
-		free(line);
-		return (NULL);
-	}
-	return (line);
-}
-
-/**
- * split_line - Split a line into tokens
- * @line: Input string
- *
- * Return: Null-terminated array of strings
- */
-char **split_line(char *line)
-{
-	int bufsize = 64, i = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
-
-	if (!tokens)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
+		if (is_executable(cmd))
+			return strdup(cmd);
+		return NULL;
 	}
 
-	token = strtok(line, " \t\r\n");
+	path_env = getenv("PATH");
+	if (!path_env || path_env[0] == '\0')
+		return NULL;
+
+	path_dup = strdup(path_env);
+	if (!path_dup)
+		return NULL;
+
+	token = strtok(path_dup, ":");
 	while (token)
 	{
-		tokens[i++] = token;
-
-		if (i >= bufsize)
+		len = strlen(token) + strlen(cmd) + 2;
+		full_path = malloc(len);
+		if (!full_path)
 		{
-			bufsize += 64;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
-			{
-				perror("realloc");
-				exit(EXIT_FAILURE);
-			}
+			free(path_dup);
+			return NULL;
 		}
-		token = strtok(NULL, " \t\r\n");
+		snprintf(full_path, len, "%s/%s", token, cmd);
+
+		if (is_executable(full_path))
+		{
+			free(path_dup);
+			return full_path;
+		}
+		free(full_path);
+		token = strtok(NULL, ":");
 	}
-	tokens[i] = NULL;
-	return (tokens);
+
+	free(path_dup);
+	return NULL;
 }
 
-/**
- * execute - Run a command
- * @args: Tokenized command
- *
- * Return: 1 to continue loop, 0 to exit
- */
 int execute(char **args)
 {
 	pid_t pid;
 	int status;
+	char *cmd_path;
 
 	if (args[0] == NULL)
-		return (1);
+		return 1;
 
 	if (handle_builtin(args) == 0)
-		return (0);
+		return 0;
+
+	cmd_path = find_command(args[0]);
+	if (!cmd_path)
+	{
+		/* Command not found, print error and do NOT fork */
+		dprintf(STDERR_FILENO, "%s: No such file or directory\n", args[0]);
+		return 1;
+	}
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execvp(args[0], args) == -1)
-		{
-			perror("execvp");
-		}
+		execve(cmd_path, args, environ);
+		perror("execve");
+		free(cmd_path);
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
 	{
 		perror("fork");
+		free(cmd_path);
+		return 1;
 	}
 	else
 	{
+		free(cmd_path);
 		waitpid(pid, &status, 0);
 	}
 
-	return (1);
+	return 1;
 }
